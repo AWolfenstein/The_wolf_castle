3c9be52f5daada48ddc42f96997dc688778d5fbb
AWolfenstein <alexsupa@mail.ru>
2020-11-17 15:20:12 +0300

    code_color

--- frontend/src/components/AdsBlock/AdsBlock.jsx
+++ frontend/src/components/AdsBlock/AdsBlock.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import {Container} from '@material-ui/core';
+
 const useStyles = makeStyles(() => ({
-  container: {
-    height: '100px',
-    backgroundColor: '#181834cf',
-    borderRadius: ' 0px 0px 20px 20px',
-    boxShadow: '1px 6px 15px 1px rgba(250,127,250,0.8);',
-  },
+    container: {
+        height: '100px',
+        backgroundColor: '#181834cf',
+        borderRadius: ' 0px 0px 20px 20px',
+        boxShadow: '1px 6px 15px 1px rgba(250,127,250,0.8);',
+    },
 }));
 const AdsBlock = () => {
-  const classes = useStyles();
-  const body = (
-    <Container children className={classes.container}>
-      fdesf
-    </Container>
-  );
-  return body;
+    const classes = useStyles();
+    const body = (
+        <Container children className={classes.container}>
+            fdesfc
+        </Container>
+    );
+    return body;
 };
 
 export default AdsBlock;
